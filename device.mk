b55363019387f4022061df706d534cd50b3c14ca
NisseCool <nils.farving@live.se>
2017-10-31 13:24:25 +0100

    Revert "sagit: more fingerprint services"
    This reverts commit 984ee3d140413e6326e3667f0ce52de3bdb84cf6.

--- device.mk
+++ device.mk
@@ -161,9 +161,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service \
-    android.hardware.biometrics.fingerprint@2.1-service \
-    fingerprintd
+    android.hardware.biometrics.fingerprint@2.0-service
 
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
