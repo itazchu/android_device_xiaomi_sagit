ed85857da052e40b52801f567c901447342f3a0e
NisseCool <nils.farving@live.se>
2017-11-05 17:29:38 +0100

    cm hardware -> lineage hardware

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -138,12 +138,9 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
-# CM Hardware
-BOARD_HARDWARE_CLASS += \
-    hardware/cyanogen/cmhw \
-    $(DEVICE_PATH)/cmhw
-BOARD_USES_CYANOGEN_HARDWARE := true
-TARGET_TAP_TO_WAKE_NODE :=  "/proc/touchpanel/double_tap_enable"
+# Lineage Hardware
+BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/lineagehw
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # CNE and DPM
 BOARD_USES_QCNE := true
