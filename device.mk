09118ab87df43b2ab72b63d6b925eccf7480e79d
NisseCool <nils.farving@live.se>
2017-12-08 22:58:53 +0100

    rootdir: kernel love

--- device.mk
+++ device.mk
@@ -308,6 +308,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.msm.usb.configfs.rc \
     init.qcom.rc \
+    init.qcom.power.rc \
     init.qcom.sh \
     init.qcom.sensors.sh \
     init.qcom.usb.rc \
