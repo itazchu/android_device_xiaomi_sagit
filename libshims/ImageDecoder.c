75d331545ee7af272af73daf2b7d0c0b11baf42b
NisseCool <nils.farving@live.se>
2017-12-25 18:06:57 +0000

    Fix camera (Thanks @Verevka)

--- libshims/ImageDecoder.c
+++ libshims/ImageDecoder.c
@@ -1,1 +1,3 @@
 extern void _ZN14SkImageDecoder10DecodeFileEPKcP8SkBitmap11SkColorTypeNS_4ModeEPNS_6FormatE() {}
+extern void _ZNK8SkBitmap10lockPixelsEv() {}
+extern void _ZNK8SkBitmap12unlockPixelsEv() {}
